b6927140b294940a42e085dd9968f08df45653dc
frankvic03 <frankviva0307@gmail.com>
2023-09-16 16:20:31 -0500

    feature/register subido

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import RegisterScreen from './views/register/RegisterScreen.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RegisterScreen/>
   </React.StrictMode>
 );
 
