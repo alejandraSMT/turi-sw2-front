d8f285393e609c4afd8714235926c8006eb739dc
frankvic03 <frankviva0307@gmail.com>
2023-09-23 15:38:01 -0500

    Creacion DetailElement

---
+++ src/views/DetailElement/DetailElement.js
@@ -0,0 +1,18 @@
+
+import CardElement from "./components/CardElement.js"
+
+
+function DetailElement(){
+    return(
+        
+        <section class="vh-100" >
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <CardElement/>
+        </section>
+    );
+}
+
+export default DetailElement;
