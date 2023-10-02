f59b62a6a914c69bdc4cc90ced107a40a52fca16
frankvic03 <frankviva0307@gmail.com>
2023-10-01 21:18:04 -0500

    Actualizacion

--- src/views/Itinerary/Itinerary.js
+++ src/views/Itinerary/Itinerary.js
@@ -6,28 +6,29 @@ import React, { useState } from 'react';
 import Day from './components/Day.js';
 
 function Itinerary() {
-
+    
     const [numberOfDays, setNumberOfDays] = useState(''); // Estado para la cantidad de días
-
+    const [arrayDays, setArrayDays] = useState([]); // Estado para el array de dias
+    
     // Función para generar los días
     const handleGenerateClick = () => {
         // Parsea la cantidad de días a un número
-        const cantDays = parseInt(numberOfDays, 10);
+        const cantDays = numberOfDays;
 
         // Verifica si la entrada es un número válido y mayor que cero
         if (!isNaN(cantDays) && cantDays > 0) {
             // Genera un arreglo de números del 1 al número de días ingresados
-            const generatedDays = Array.from({ length: cantDays }, (_, index) => index + 1);
+            const arrayDays = Array.from({ length: cantDays }, (_, index) => index + 1);
 
             // Actualiza el estado con los días generados
-            setGeneratedDays(generatedDays);
+            setArrayDays(arrayDays);
         }
     };
 
-    const [generatedDays, setGeneratedDays] = useState([]); // Estado para los días generados
-
+  
     return (
         <Container>
+            <br/>
             <h1 id="tittle">Crea tu itinerario ahora con Turi!</h1>
             <h3 id="subtittle">Ingrese la cantidad de días para generar:</h3>
             <FormGroup id="cardDaysContainer">
@@ -35,21 +36,23 @@ function Itinerary() {
                 <FormControl
                     type="number"
                     id="days"
-                    className="mr-2"
+                    
                     value={numberOfDays}
                     onChange={(e) => setNumberOfDays(e.target.value)}
                 />
-                <Button onClick={handleGenerateClick}>Generar</Button>
+                <Button onClick={handleGenerateClick} id="Buttons">Generar</Button>
             </FormGroup>
             <Container>
-                {/* Mapea y renderiza los componentes "Day" generados */}
-                {generatedDays.map((day, index) => (
-                    <div>
+                {/* Mapea y renderiza los componentes "Day" del array de dias */}
+                {arrayDays.map((day, index) => (
+                    <div key={index}>
                     
                     <Day key={index} dayNumber={day} index={index}/>
+                    
                     </div>
                 ))}
             </Container>
+            <Button id="ButtonSave">Guardar</Button>
         </Container>
     );
 }
--- src/views/Itinerary/components/Day.js
+++ src/views/Itinerary/components/Day.js
@@ -6,47 +6,95 @@ import React, { useState, useEffect } from 'react';
 
 
 function Day(props){
-const { content, index } = props;
-const favorites = ["Lugar 1","Lugar 2", "Lugar 3"];
+const { index } = props;
+const favorites = ["Tanta","Parque de las Leyendas", "Pizza Raul","Larcomar","Jockey Plaza","Parque de las Aguas"];
 const times = ["Dia","Tarde","Noche"];
 
+const [itinerario, setItinerario] = useState([]);
+const [selectedFavorite, setSelectedFavorite] = useState(favorites[0]);
+const [selectedTime, setSelectedTime] = useState(times[0]);
+const [arrayDia, setArrayDia] = useState(["","",""]);
+
+const AddFavorite = () => {
+  
+
+  // la posicion 1 debe ser la actividad del dia, la 2 de la tarde y la 3 de la noche
+  
+  
+  if(selectedTime === "Dia"){
+    setArrayDia([selectedFavorite, arrayDia[1], arrayDia[2]]);
+    //arrayDia[0] = selectedFavorite;
+  }else if(selectedTime === "Tarde"){
+    //arrayDia[1] = selectedFavorite;
+    setArrayDia([arrayDia[0],selectedFavorite, arrayDia[2]]);
+  }else if(selectedTime === "Noche"){
+    //arrayDia[2] = selectedFavorite;
+    setArrayDia([arrayDia[0],arrayDia[1],selectedFavorite]);
+
+  }
+  itinerario[index] = arrayDia;
+  setItinerario(itinerario);
+  setSelectedFavorite(selectedFavorite);
+  setSelectedTime(selectedTime);
+  console.log(itinerario[index]);
+  
+};
+
+
     return(
+
        <Container>
         <div id="header-day">
-        <h2>Dia {index + 1}</h2>
+        <h1 id="Tittle">Dia {index + 1}</h1>
         
         
-        <select>
+        <select id="selected"  onChange={(e) => setSelectedFavorite(e.target.value)}>
             
               {favorites.map((favorite, index) => (
                 <option key={index} value={favorite}>
                   {favorite}
                 </option>
-              ))}
-              
+              ))}   
         </select>
-        <select>
+
+        <select  id="selected" value={selectedTime} onChange={(e) => setSelectedTime(e.target.value)}> 
             
               {times.map((time, index) => (
                 <option key={index} value={time}>
                   {time}
                 </option>
               ))}
-              
         </select>
+
+        <Button onClick={AddFavorite} id="Buttons"> Agregar </Button>
         </div>
-        <h2>Dia:</h2>
-        <br/>
+        <Container id="ContainerDay">
+        <div id="TimeTitle">
+        
+        <h2>Dia</h2>
+        <img src="https://www.vippng.com/png/full/164-1649488_minus-sol-y-nubes-rayos-de-sol-dibujos.png" id="TimeImage"/>
+        <div id="rptFavorite">{arrayDia[0]}</div>
+        </div>
+        
         
         <br/>
-        <h3>Tarde:</h3>
-        <br/>
+        <div id="TimeTitle">
+        <h3 >Tarde</h3>
+        <img src="https://cdn-icons-png.flaticon.com/512/2972/2972516.png" id="TimeImage"/>
+        <div id="rptFavorite">{arrayDia[1]}</div>
+        </div>
         
+       
         <br/>
-        <h3>Noche:</h3>
-        
+        <div id="TimeTitle">
+        <h3>Noche</h3>
+        <img src="https://cdn-icons-png.flaticon.com/512/3026/3026346.png" id="TimeImage"/>
+        <div id="rptFavorite">{arrayDia[2]}</div>
+        </div>
+       
         <br/>
         <br/>
+        </Container>
         </Container>
     );
 }
