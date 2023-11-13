46cd2f9a8d3d8737938daf9461c5b8ac83176f07
Alejandra <20201923@aloe.ulima.edu.pe>
2023-11-12 22:34:26 -0500

    itinerary modify complete minor fix

--- src/views/modifyItinerary/components/AddedPlaceCard.js
+++ src/views/modifyItinerary/components/AddedPlaceCard.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import "../style/AddedPlaceCard.css"
 
-function AddedPlaceCard({ added, setSelected, selected}) {
+function AddedPlaceCard({ added}) {
     
     const navigate = useNavigate()
 
